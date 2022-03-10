18cf23635d24efbb86f9d7795f678540b9179f28
NgnHoaiTan <hoaitanmd@gmail.com>
2022-03-10 20:46:57 +0700

    organize structure of project

---
+++ client/src/ClientSite/Components/LandingPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header'
+import bglandingpage from '../../../Images/bglandingpage.jpg'
+const LandingPage=()=>{
+    return(
+        <>
+            <Header />
+            <img src={bglandingpage} />
+        </>
+        
+        
+    )
+}
+export default LandingPage;
